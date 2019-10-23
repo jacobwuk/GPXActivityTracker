20aaba407e26727547b4e3454884c4a10372eef1
Jacob Wukovits <jacobwuk@buffalo.edu>
2019-10-23 13:10:06 -0400

    Initialize file

---
+++ track_distance.py
@@ -0,0 +1,11 @@
+"""
+Given a .gpx file containing a set of tracks, returns the cumulative distance
+traversed. 
+
+WARNING: This does not check for continuity in the track!
+         The output distance assumes that all pt->pt transitions are valid.
+"""
+
+def funcname(self, parameter_list):
+  pass
+
