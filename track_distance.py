24cca5bf1b3c7699a50dc15ac17aef29ba5fc0c4
Jacob Wukovits <jacobwuk@buffalo.edu>
2019-12-23 20:41:10 -0500

    Update comments for clarity

--- track_distance.py
+++ track_distance.py
@@ -1,31 +1,42 @@
-"""
-Given a .gpx file containing a set of tracks, returns the cumulative distance
-traversed. 
-
-WARNING: This does not check for continuity in the track!
-         The output distance assumes that all pt->pt transitions are valid.
-"""
 import math as m
 from lxml import etree as et
 # Average radius of Earth, in meters
-# NASA, volumetric mean radius from: https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+# NASA, volumetric mean radius: https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
 EARTH_RADIUS = 6371000
 
 def cumulative_track_distance(filename):
+  """Computes the cumulative distance traversed in a .gpx file.
+
+  Sums up the distance between each point in the file. NOTE: Currently all 
+  point->point transistions are treated as valid: this method does not check for continuity.
+  
+  Args:
+    filename: The name of a .gpx file.  
+  """
+  
+  # figure out file not found error handling here 
+
+
   tree = et.parse(filename)
   root = tree.getroot()
   # root currently points towards {default}gpx 
   # extract the root node of the sub-tree containing the tracks
   trkRoot = root.find('trk', root.nsmap)
-  # hold the extracted (lat, long) as a tuple in decimal degrees
+  # hold the extracted (lat, long) as a tuples in decimal degrees
   coords = []
+
   # Activities can be segmented, accumulate points from all segments 
   # TODO: Decide how to deal with "disjoint" segments. For now, the segments
   # are treated as continuous, but in reality this can introduce "jumps"
-  # if the distance between the end and start of the next seg is large.
+  # if the distance between the end and start of the next seg is large. 
+  # (tracking was paused on the device, segments were deleted from the .gpx)
   # "Large" is just relative maaan, so may want to have some statistics
-  # to detect anomolous separation (avg dist moved, +- some % of that is ok) 
+  # to detect anomolous separation (avg dist moved, +- some % of that is ok)
+
+  # Each trkseg is a set of points, Suunto Ambit 3 creates a new trkseg after a pause  
   for seg in trkRoot.findall('trkseg', root.nsmap):
+    # In the future, may want to calculate the distance traversed in each segment 
+    # separately and then sum the segments
     for pt in seg.findall('trkpt', root.nsmap):
       # trkpt tags contain the values lat, lon: <trkpt lat= , lon= >
       coords.append( (float(pt.get('lat')), float(pt.get('lon'))) )
@@ -41,22 +52,22 @@ def cumulative_track_distance(filename):
 def haversine_distance(cordA, cordB):
   """
   Returns the great circle distance between two decimal gps coordinates.
-  
+
   Calculated by the haversine formula: https://en.wikipedia.org/wiki/Haversine_formula 
   
-  @param cordA a (lat, long) tuple of point A 
-  @param cordB a (lat, long) tuple of point B
-  @return the distance, in meters, between the two points
+  Args:
+      cordA: A (float lat, float long) coordinate tuple of the starting point. 
+      cordB: A (float lat, float long) coordinate tuple of the end point.
+  
+  Returns:
+    The distance, in meters, between the two points
   """
   d = 2.0 * EARTH_RADIUS * haversine(cordA, cordB)
   return d
 
-# The Haversine function
-def haversine(radA, radB):
-  """
-  Returns the haversine-based component of the distance calculation 
-  """
-  xA, yA = m.radians(radA[0]), m.radians(radA[1])
-  xB, yB = m.radians(radB[0]), m.radians(radB[1])
-  hav = m.sin((xB - xA) / 2)**2 + m.cos(xA) * m.cos(xB) * m.sin((yB - yA) / 2)**2
+def haversine(ptA, ptB):
+  ''' Returns the haversine component per: https://en.wikipedia.org/wiki/Haversine_formula '''
+  radxA, radyA = m.radians(ptA[0]), m.radians(ptA[1])
+  radxB, radyB = m.radians(ptB[0]), m.radians(ptB[1])
+  hav = m.sin((radxB - radxA) / 2)**2 + m.cos(radxA) * m.cos(radxB) * m.sin((radyB - radyA) / 2)**2
   return m.asin(m.sqrt(hav))
