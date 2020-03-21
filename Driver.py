2185d80a31facbe348474002a2bce1c642219e0f
Jacob Wukovits <jacobwuk@buffalo.edu>
2020-03-20 23:09:02 -0400

    Create main(), fix glob.glob arg in specified directory case
    Previous expression sourceDir/'*.gpx' was incorrect syntax

--- Driver.py
+++ Driver.py
@@ -3,7 +3,7 @@ import track_distance
 from pathlib import Path
 
 def locate_gpx_files(sourceDir='.'):
-  """ Prints the distance covered in all .gpx files in the current dir (and sub-dirs)
+  """ Returns all .gpx files in the current or specified directory, recursively
   """
   if sourceDir == '.':
     # Default handling: accumulate all .gpx files, including those in sub-dirs
@@ -12,16 +12,19 @@ def locate_gpx_files(sourceDir='.'):
     #check whether the path is valid
     # if it is, return all gpx files with filepath as a stem
     if Path(sourceDir).exists():
-      return glob.glob(sourceDir/'**/*.gpx', recursive=True)
+      return glob.glob(str(sourceDir) + '/*.gpx', recursive=True)
     else:
       print(sourceDir, 'is not a valid directory!')
 
-if __name__ == '__main__':  
+def main():
   # Scan the current directory (and any sub-dirs) for .gpx files
     #TODO: when settings/storage is figured out this can be converted to read
-    # the specified storage location (and check that it exists) by kwargs
-  gpxFiles = locate_gpx_files()
+    # the specified storage location (and check that it exists -> use 'with' is a good idea)
+  gpxFiles = locate_gpx_files(Path(Path.home(), '.gpx'))
   print('{0} file(s) found'.format(len(gpxFiles)))
   for p in gpxFiles:
-    f = open(p)
-    print('file: {0}\nmeters covered: {1}\n'.format(Path(p).stem, track_distance.cumulative_track_distance(f)))
+    with open(p) as f:
+      print('file: {0}\nmeters covered: {1}\n'.format(Path(p).stem, track_distance.cumulative_track_distance(f)))
+
+if __name__ == '__main__':  
+  main()
